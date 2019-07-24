54c88cc3177d7e4483eb6d4c1d78e3ce7959257b
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-25 00:14:15 +0800

    modify files

--- PDFbinder.py
+++ PDFbinder.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-# 合并多个pdf为一个
+#
+#    Author: Shieber
+#    Date: 2019.07.24
+#
+#                             APACHE LICENSE
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#                            Function Description
+#    transfer txt file to pdf file.
+#    combine multiple pdf files into sigle one pdf.
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import sys, PyPDF2
 from os.path import basename
